0580a19cc8640589424f44bcedea9286606768a9
xiaotiangu <89456183+xiaotianr@users.noreply.github.com>
2023-10-10 16:56:35 +0800

    Update train.py

--- server/MMGraph/train.py
+++ server/MMGraph/train.py
@@ -50,8 +50,6 @@ def loadAdjs(tfids,Kmers):
         adj_sim = scipy.sparse.load_npz(filename)
         filename = path+"/"+tfids + "_encode_" + strsize + "_" + "adj_coo" + ext
         adj_coo = scipy.sparse.load_npz(filename)
-        filename = path+"/"+tfids + "_encode_" + strsize + "_" + "adj_jacc" + ext
-        adj_jacc = scipy.sparse.load_npz(filename)
     else:
         adj_inclu, adj_sim, adj_coo,adj_jacc, labels = generateAdjs(tfids,Kmers)
         filename = path+"/"+tfids + "_encode_" + strsize + "_" + "adj_inclu" + ext
@@ -60,13 +58,11 @@ def loadAdjs(tfids,Kmers):
         scipy.sparse.save_npz(filename, adj_sim)
         filename = path+"/"+tfids + "_encode_" + strsize + "_" + "adj_coo" + ext
         scipy.sparse.save_npz(filename, adj_coo)
-        filename = path+"/"+tfids + "_encode_" + strsize + "_" + "adj_jacc" + ext
-        scipy.sparse.save_npz(filename, adj_jacc)
         labelname = path+"/"+tfids + "_encode_" + strsize + "_" + "labels.txt"
         np.save(labelname,labels)
     return adj_inclu, adj_sim, adj_coo,adj_jacc, labels
 ###################training the MMGraph tools########################################	
-def one_task(name,Kmers,adjs_0, adjs_1, adjs_2,adjs_3, idx_train, idx_val, idx_test, idx_all, labels, options, save=False):
+def one_task(name,Kmers,adjs_0, adjs_1,adjs_3, idx_train, idx_val, idx_test, idx_all, labels, options, save=False):
     n = len(labels)
     start_time = time.time()
     test_mask = np.zeros(n,dtype=np.int)
@@ -80,23 +76,18 @@ def one_task(name,Kmers,adjs_0, adjs_1, adjs_2,adjs_3, idx_train, idx_val, idx_t
     if options['model'] == 'gcn':
         support0 = [preprocess_adj(adj) for adj in adjs_0]
         support1 = [preprocess_adj(adj) for adj in adjs_1]
-        support2 = [preprocess_adj(adj) for adj in adjs_2]
         num_support = len(adjs_0)
     feature0 = scipy.sparse.identity(adjs_0[0].shape[0])
     feature0 = preprocess_features(feature0)
     feature1 = scipy.sparse.identity(adjs_1[0].shape[0])
     feature1 = preprocess_features(feature1)
-    feature2 = scipy.sparse.identity(adjs_2[0].shape[0])
-    feature2 = preprocess_features(feature2)
     feature3 = adjs_3 ##inclusive array
 
     placeholders = {
         'support0':[tf.sparse_placeholder(tf.float32) for _ in range(num_support)],
         'support1':[tf.sparse_placeholder(tf.float32) for _ in range(num_support)],
-        'support2':[tf.sparse_placeholder(tf.float32) for _ in range(num_support)],
         'feature0':tf.sparse_placeholder(tf.float32, shape=None),
         'feature1':tf.sparse_placeholder(tf.float32, shape=None),
-        'feature2':tf.sparse_placeholder(tf.float32, shape=None),
         'feature3':tf.placeholder(tf.float32, shape=None), #inclusive
         'labels':tf.placeholder(tf.float32, shape=None),
         'labels_mask': tf.placeholder(tf.bool),
@@ -110,25 +101,25 @@ def one_task(name,Kmers,adjs_0, adjs_1, adjs_2,adjs_3, idx_train, idx_val, idx_t
     sess = tf.Session(config=configs)
     
     # # define model evaluation function
-    def evaluate(feature0, feature1,feature2, feature3, support0, support1,support2, label, mask, placeholders):
+    def evaluate(feature0, feature1, feature3, support0, support1, label, mask, placeholders):
         feed_dict_val = construct_feed_dict(
-            feature0, feature1,feature2,feature3, support0, support1,support2, label, mask, placeholders)
+            feature0, feature1,feature3, support0, support1, label, mask, placeholders)
         loss,preds,labels, _, _, seq_hidden, kmer_hidden = sess.run([model.loss, model.preds, model.labels, model.debug, model.activations_3, model.seq_hidden, model.kmer_hidden], feed_dict=feed_dict_val)
         return loss,preds,labels,seq_hidden, kmer_hidden
     sess.run(tf.global_variables_initializer())
     cost_val=[]
     # train model
-    feed_dict = construct_feed_dict(feature0, feature1, feature2, feature3, support0, support1,support2, labels, train_mask, placeholders)
+    feed_dict = construct_feed_dict(feature0, feature1, feature3, support0, support1, labels, train_mask, placeholders)
     feed_dict.update({placeholders['dropout']:0.2})
     for epoch in tqdm(range(options['epochs']+1)):
         outs = sess.run([model.opt_op, model.loss, model.preds, model.debug, model.activations_3,model.seq_hidden, model.kmer_hidden], feed_dict=feed_dict)
         if epoch % 2 == 0:
-            val_loss, preds, labels,_,_= evaluate(feature0, feature1,feature2,feature3, support0, support1, support2, labels, val_mask, placeholders)
+            val_loss, preds, labels,_,_= evaluate(feature0, feature1,feature3, support0, support1, labels, val_mask, placeholders)
             # print(preds)
             val_auc,fpr,tpr,thresholds = com_auc(labels,preds,idx_val)
             print("epoch %d: valid loss = %f, val_auc = %f" %(epoch, val_loss, val_auc))
         if epoch == options['epochs']:
-            test_loss, preds, labels,seq_hidden, kmer_hidden= evaluate(feature0, feature1,feature2,feature3, support0, support1, support2, labels, test_mask, placeholders)
+            test_loss, preds, labels,seq_hidden, kmer_hidden= evaluate(feature0, feature1,feature3, support0, support1, labels, test_mask, placeholders)
             test_auc,fpr,tpr,thresholds = com_auc(labels, preds,idx_test)
             print("epoch %d: test_auc = %f" %(epoch, test_auc))
             path = './download/'+name 
@@ -167,24 +158,22 @@ def motif_task(args):
     # tfid = args.dataset
     tfid = args.hash
     local_weights = [0.1]
-    adj_inclu, adj_sim, adj_coo, adj_jacc, labels = loadAdjs(tfid,Kmers)
+    adj_inclu, adj_sim, adj_coo, labels = loadAdjs(tfid,Kmers)
     n = len(labels)
     #########################################
     aucs =[]
     adj0 = 1* adj_coo # 5.0
     adj1 = adj_sim
     adj2 = adj_inclu.toarray()
-    adj3 = adj_jacc
     adjs_0=[adj0]#coexsiting edge
     adjs_1=[adj1]# similarity edge
-    adjs_2=[adj3]# jaccard edge
     adj3 = adj2 # inclusive edge
     idx_all = np.array([i for i in range(n)],dtype='int')
     idx_train = idx_all[:int(0.8*n)]
     idx_val = idx_all[int(0.8*n):int(0.9*n)]
     idx_test = idx_all[int(0.9*n):]
     options['hidden_shape'] = adj1.toarray().shape
-    one_task(args.hash,Kmers,adjs_0, adjs_1,adjs_2,adj3, idx_train, idx_val, idx_test, idx_all, labels, options, True)
+    one_task(args.hash,Kmers,adjs_0, adjs_1,adj3, idx_train, idx_val, idx_test, idx_all, labels, options, True)
 ##########################main##############
 if __name__=='__main__':
     start_time = time.time()
